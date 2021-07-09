76562c8fc2009c65acf9d121752b53969b6ba36d
Glynn Bird <glynn.bird@gmail.com>
2021-07-09 16:12:57 +0100

    improved string handling in table

--- components/DocTable.vue
+++ components/DocTable.vue
@@ -1,7 +1,7 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="docs"
+    :items="truncatedDocs"
     :items-per-page="itemsPerPage"
     class="elevation-1"
     hide-default-footer
@@ -31,13 +31,29 @@ export default {
       const uniqueKeys = new Set()
       for(const doc of this.docs) {
         for(const k of Object.keys(doc)) {
-          if (k !== '_rev' && typeof doc[k] !== 'object') {
-            uniqueKeys.add(k)
+          const t = typeof doc[k]
+          if (k !== '_rev' && t !== 'object') {
+            if (t !== 'string' || doc[k].length > 0) {
+              uniqueKeys.add(k)
+            }
           }
         }
       }
       // convert set to array
       return [...uniqueKeys].map((k) => { return { text: k, value: k }})
+    },
+    truncatedDocs: function () {
+      const retval = []
+      for(const doc of this.docs) {
+        const obj = JSON.parse(JSON.stringify(doc))
+        for(const k of Object.keys(obj)) {
+          if (k !== '_id' && typeof obj[k] === 'string' && obj[k].length > 20) {
+            obj[k] = obj[k].substr(0, 20) + '...'
+          }
+        }
+        retval.push(obj)
+      }
+      return retval
     }
   }
 }
