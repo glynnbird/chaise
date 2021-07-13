9f0f82c2efdb1234f8b8338c09d65d9ee174e199
Glynn Bird <glynn.bird@gmail.com>
2021-07-13 09:21:37 +0100

    tidy up

---
+++ components/AppBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar color="blue-grey lighten-4" app dense v-if="$store.state.session.currentService">
+    <v-app-bar-nav-icon @click.stop="$emit('revealNavDrawer')"></v-app-bar-nav-icon>
+    <v-toolbar-title>
+      <Breadcrumb :title="$store.state.session.currentService.name" />
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <!-- add document icon -->
+    <v-chip v-if="$store.state.session.currentService.readonly">
+      Read only
+    </v-chip>
+    <v-btn nuxt to="/doc_add" icon v-if="!$store.state.session.currentService.readonly && $store.state.cache.currentDB && !$store.state.cache.currentID">
+      <v-icon>mdi-file-plus</v-icon>
+    </v-btn>
+    <v-btn nuxt to="/db_add" icon v-if="!$store.state.session.currentService.readonly && !$store.state.cache.currentDB">
+      <v-icon>mdi-database-plus</v-icon>
+    </v-btn>  
+    <v-btn nuxt to="/db_query" icon>
+      <v-icon>mdi-database-search</v-icon>
+    </v-btn>   
+    <v-btn nuxt to="/settings" icon>
+      <v-icon>mdi-cog</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,7 @@
 <template>
   <v-app>
     <NavDrawer ref="drawer" />
-    <v-app-bar color="blue-grey lighten-4" app dense v-if="$store.state.session.currentService">
-      <v-app-bar-nav-icon @click.stop="onClickNavbarReveal"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>
-        <Breadcrumb :title="$store.state.session.currentService.name" />
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <!-- add document icon -->
-      <v-chip v-if="$store.state.session.currentService.readonly">
-        Read only
-      </v-chip>
-      <v-btn nuxt to="/doc_add" icon v-if="!$store.state.session.currentService.readonly && $store.state.cache.currentDB && !$store.state.cache.currentID">
-        <v-icon>mdi-file-plus</v-icon>
-      </v-btn>
-      <v-btn nuxt to="/db_add" icon v-if="!$store.state.session.currentService.readonly && !$store.state.cache.currentDB">
-        <v-icon>mdi-database-plus</v-icon>
-      </v-btn>  
-      <v-btn nuxt to="/db_query" icon>
-        <v-icon>mdi-database-search</v-icon>
-      </v-btn>   
-      <v-btn nuxt to="/settings" icon>
-        <v-icon>mdi-cog</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <AppBar @revealNavDrawer="onClickNavDrawerReveal" />
     <v-main>
       <Alert />
       <v-container fluid>
@@ -36,11 +13,8 @@
 
 <script>
 export default {
-  data () {
-    return { }
-  },
   methods: {
-    onClickNavbarReveal: function () {
+    onClickNavDrawerReveal: function () {
       this.$refs.drawer.show = !this.$refs.drawer.show
     }
   }
